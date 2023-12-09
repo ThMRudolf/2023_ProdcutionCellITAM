1bf81d157df3b04c8489cf5b022ddfbedbd9ec7d
ThMRudolf <thomas.rudolf@itam.mx>
2023-12-09 08:29:26 -0600

    added sensors for Y, Z and spindle

--- CNCTMp1/C/test_hall.ino
+++ CNCTMp1/C/test_hall.ino
@@ -1,13 +1,34 @@
-const int HS_PIN1 = A0;
-const int HS_PIN2 = A1;
-const int HS_PIN3 = A2;
+# drive axis X
+const int HS_PIN11 = A0;
+const int HS_PIN12 = A1;
+const int HS_PIN13 = A2;
 
+# drive axis Y
+const int HS_PIN21 = A3;
+const int HS_PIN22 = A4;
+const int HS_PIN23 = A5;
+
+# drive axis Z
+const int HS_PIN31 = A6;
+const int HS_PIN32 = A7;
+const int HS_PIN33 = A8;
+
+# drive axis Spindle
+const int HS_PIN41 = A9;
+const int HS_PIN42 = A10;
+const int HS_PIN43 = A11;
+
+
+# Comunication to ??
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-    int arr[3] = {HS_PIN1,HS_PIN2,HS_PIN3};
+    int arr[12] = {HS_PIN11,HS_PIN12,HS_PIN13,
+                    HS_PIN21,HS_PIN22,HS_PIN23, 
+                    HS_PIN31,HS_PIN32,HS_PIN33, 
+                    HS_PIN41,HS_PIN42,HS_PIN43};
     readMultipleCurrents(arr);
 }
 
